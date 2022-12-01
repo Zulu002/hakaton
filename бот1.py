343dc0779add65c2e12908333a493fac0231b99b
Foo0s <oraproavp@list.ru>
2022-12-02 00:35:58 +0300

    upload

--- бот1.py
+++ бот1.py
@@ -1,1 +1,18 @@
-"""Дальше бога нет."""
+"""Дальше бога нет.
+   ссылка на бота -> http://t.me/ndflDok_bot"""
+
+import telebot
+
+token = "5614187069:AAFxZNIR2tNpFLFWQ2IirgubkPBQNQzLMos"
+ndflbot = telebot.TeleBot(token)
+
+'''Начало бота - команда start.'''
+@ndflbot.message_handler(commands=["start"])
+def start(message):
+    ndflbot.send_message(message.chat.id, "Привет.")
+
+
+
+
+'''Работа в режиме нон-стоп.'''
+ndflbot.polling(none_stop=True)
